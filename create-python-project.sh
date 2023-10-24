948fa7357270c8d4162e3735b8dfb1baea371dd7
Akkusativ23 <s6arweis@uni-bonn.de>
2023-10-24 23:20:19 +0200

    initial commit

--- create-python-project.sh
+++ create-python-project.sh
@@ -1,6 +1,33 @@
 #!/bin/bash
 
-#create and activate venv
+# Packages flag
+# Initialize variables
+packages=()
+
+# Process command-line arguments
+while [[ "$#" -gt 0 ]]; do
+    case "$1" in
+        -p|--packages)
+            shift 1
+            while [[ "$1" != "" && ! "$1" =~ ^-.*$ ]]; do
+                packages+=("$1")
+                shift 1
+            done
+            ;;
+        *)
+            echo "Unknown option: $1"
+            exit 1
+            ;;
+    esac
+done
+
+# Install the specified packages using pip
+for package in "${packages[@]}"; do
+    pip install "$package"
+done
+
+# Project creation
+# create and activate venv
 python -m venv venv
 source ./venv/bin/activate
 
