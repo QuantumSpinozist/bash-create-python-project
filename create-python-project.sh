6002e5423c4bac123ac65a1cf9e19dae73919b8d
Akkusativ23 <s6arweis@uni-bonn.de>
2023-10-24 22:58:13 +0200

    initial commit

--- create-python-project.sh
+++ create-python-project.sh
@@ -1,6 +1,32 @@
-python -m venv venv
-source venv/bin/activate
+#!/bin/bash
 
+#create and activate venv
+python -m venv venv
+source ./venv/bin/activate
+
+# pip installs
+pip install pytest
+
+pip freeze > requirements.txt
+
+
+#create directories
 mkdir code
-mkdir test
+mkdir tests
+
+#create main file
+printf "if __name__ ==  "main.py":\n    pass" > code/main.py
+#create test file
+printf "import pytest" > tests/test.py
+
+# create local git repo
+'venv' > .gitignore
+
+git init
+git add .
+git commit -m "initial commit"
+
+
+
+
 
